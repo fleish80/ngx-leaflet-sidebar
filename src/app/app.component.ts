cf20ff8b6110ed5d9984c28329a4ba96037062c5
Dmitry Fleish <DXF0530@corp.aeroscout.com>
2019-03-15 13:01:00 +0200

    add map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MapOptions, tileLayer, latLng } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  options: MapOptions
+
+  ngOnInit() {
+    this.options = {
+      layers: [
+        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      ],
+      zoom: 5,
+      center: latLng(46.879966, -121.726909)
+    };
+  }
   title = 'ngx-leaflet-sidebar';
 }
